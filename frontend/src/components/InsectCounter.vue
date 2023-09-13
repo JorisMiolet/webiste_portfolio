d17a4512f98ad4717b579b3932c27fabd542b43a
Bas Bolier <b.bolier@hva.nl>
2023-09-13 20:24:25 +0200

    Added insect counter

--- frontend/src/components/InsectCounter.vue
+++ frontend/src/components/InsectCounter.vue
@@ -1,15 +1,21 @@
 <template>
   <div>
-  <h1>Welkom bij de insect counter </h1>
-  <p v-bind:style=" counter > 1 ?'visibility: hidden' : ''">klik op de button als je een insect ziet </p>
+    <h1>{{getMessage()}}</h1>
+    <p v-bind:style=" counter > 1 ?'visibility: hidden' : ''">klik op de button als je een insect ziet </p>
   </div>
 
   <div>
-    Je hebt {{counter}} insecten gezien
+    Je hebt {{ counter }} insecten gezien
   </div>
 
-  <div v-if="counter % 2 ==0"> Je hebt een evenaantal insecten gezien</div>
+  <div v-if="counter % 2 ==0"> Je hebt een even aantal insecten gezien</div>
   <div v-else> Je hebt een oneven anatal insecten gezien</div>
+
+  <div v-if="this.counts.length > 0">
+    <ol>
+      <li v-for=" count in counts" :key="count.date"> {{ count.date }}</li>
+    </ol>
+  </div>
 
   <div>
     <button v-on:click="addInsect()">Click</button>
@@ -17,25 +23,36 @@
 </template>
 
 <script>
+import {InsectCount} from "@/models/InsectCount";
+
 export default {
   name: "InsectCounter",
-  data(){
+  data() {
     return {
-      counter : 1
+      counter: 1,
+      counts: []
     }
   },
-  methods:{
-      addInsect(){
-        this.counter++;
-      }
+  methods: {
+    addInsect() {
+      this.counter++;
+      const count = new InsectCount();
+      count.date = new Date();
+      this.counts.push(count);
+    },
+    getMessage(){
+      console.log("Zet welkom message")
+      return "Welkom bij de insect counter";
+    }
   }
 }
 </script>
 
 <style scoped>
 div {
-  border: solid black; padding: 10px; margin: 10px;
+  border: solid black;
+  padding: 10px;
+  margin: 10px;
 }
-
 
 </style>
---
+++ frontend/src/models/InsectCount.js
@@ -0,0 +1,11 @@
+
+
+export class InsectCount {
+
+    id;
+    date;
+    name;
+
+}
+
+
