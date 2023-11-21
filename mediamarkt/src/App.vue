1f1bc6f5e7ab80f97bf6993f1efb3a36b5822abf
jim <baltusj4@hva.nl>
2023-11-21 11:51:53 +0100

    deleted sidenav added EAN filter to search

--- mediamarkt/src/App.vue
+++ mediamarkt/src/App.vue
@@ -13,8 +13,5 @@ export default {
 </script>
 
 <style>
-html{
-  width: 100%;
-  height: 100%;
-}
+html, body {margin: 0; height: 100%; overflow: hidden}
 </style>
--- mediamarkt/src/components/Homepage/imageComponent.vue
+++ mediamarkt/src/components/Homepage/imageComponent.vue
@@ -1,10 +1,9 @@
 <template>
-  <sidenav-component @search="filterLaptopsbyEAN" />
   <search-component @search="filterLaptops" />
-  <div class="w-3/4 p-4 grid grid-cols-2  ml-auto mr-9 gap-4">
-    <div v-for="(laptop, index) in laptops"
+  <div class="w-3/4 p-4 mt-24 pb-28 grid grid-cols-2 h-screen ml-auto mr-9 gap-4 overflow-y-scroll">
+    <div  v-for="(laptop, index) in laptops"
          :key="index"
-         class="border border-gray-300 p-4 rounded-lg shadow-md hover:shadow-lg h-[300px]"
+         class="border border-gray-300 p-4 rounded-lg shadow-md hover:shadow-lg h-50 w-auto"
          @click="setSelectedImage(laptop)"
          :class="{ 'selected': laptop === selectedImageInfo }">
 
@@ -24,13 +23,11 @@
 import detailImage from "@/components/admin/pages/detail-image";
 import axios from "axios";
 import searchComponent from "@/components/Homepage/searchComponent.vue";
-import sidenavComponent from "@/components/Homepage/sidenavComponent.vue";
 export default {
   name: "imageComponent",
   components: {
     detailImage,
     searchComponent,
-    sidenavComponent
   },
   data() {
     return {
@@ -68,25 +65,7 @@ export default {
         //checks if searchQuery matches a laptop
         const pattern = new RegExp(`^${searchQuery}.*$`, 'i');
         this.laptops = this.originalLaptops.filter((laptop) => {
-          return pattern.test(laptop['Description / Model type']) || pattern.test(laptop['Brand']);
-        });
-
-        // If no laptops match the search query, set this.laptops to an empty array
-        if (this.laptops.length === 0) {
-          this.laptops = [];
-        }
-      }
-    },
-
-
-    filterLaptopsbyEAN(searchQuery) {
-      if (searchQuery.trim() === "") {
-        this.loadUserList(); // Reset to the original list if the search query is empty
-      } else {
-        //check if searchQuery mathes a laptop
-        const pattern = new RegExp(`^${searchQuery}.*$`);
-        this.laptops = this.originalLaptops.filter((laptop) => {
-          return pattern.test(laptop['EAN']);
+          return pattern.test(laptop['Description / Model type']) || pattern.test(laptop['Brand']) || pattern.test(laptop['EAN']);
         });
 
         // If no laptops match the search query, set this.laptops to an empty array
@@ -100,6 +79,9 @@ export default {
 </script>
 
 <style scoped>
+body, html{
+  overflow-y: hidden;
+}
 
 </style>
 
