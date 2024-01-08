e3d24d12af2792eed1fdd85901b8108d97426583
jim <baltusj4@hva.nl>
2024-01-08 11:51:22 +0100

    added backend tests

---
+++ backend/backend/src/test/java/com/mediamarkt/backend/ImageControllerTests.java
@@ -0,0 +1,9 @@
+package com.mediamarkt.backend;
+
+import com.mediamarkt.backend.models.Image;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.ResponseEntity;
+
+
+public class ImageControllerTests {
+}
