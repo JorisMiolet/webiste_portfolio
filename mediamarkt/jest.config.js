acf31c452fbd1821bc1eb6e79bb9b01449ae4e4d
Joris Mioulet <joris.mioulet@hva.nl>
2024-01-15 09:56:25 +0100

    jest jankt nogsteeds

--- mediamarkt/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    preset: '@vue/cli-plugin-unit-jest/presets/typescript',
-    testMatch: ['**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'],
-    transform: {
-        '^.+\\.vue$': 'vue-jest',
-    },
-};
